ee71c6db2b9f8510178f75bc0fd643f66b5f77df
alex t <tsuria@tut.by>
2015-12-21 17:22:48 +0100

    map home to / /todo /todolist

--- src/todoweb/src/main/java/org/alex/todoweb/controller/HomeController.java
+++ src/todoweb/src/main/java/org/alex/todoweb/controller/HomeController.java
@@ -8,14 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
 @Controller
 public class HomeController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
-    @RequestMapping(value = "/todo", method = RequestMethod.GET)
+    @RequestMapping(value = {"/todo", "/", "/todolist"}, method = RequestMethod.GET)
+
     public String showHomePage(Model model) {
         LOGGER.debug("Rendering home page");
 
